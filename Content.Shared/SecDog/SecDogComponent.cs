4c677c7f8f22011585515c9cb5a750e2c6fefc96
daxter154 <justinmwinningham@gmail.com>
2025-03-17 08:34:39 -0700

    stubbing out component and system for secdog

--- Content.Shared/SecDog/SecDogComponent.cs
+++ Content.Shared/SecDog/SecDogComponent.cs
@@ -1,6 +1,18 @@
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
 namespace Content.Shared.SecDog;
 
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(SecDogSystem))]
 public sealed partial class SecDogComponent: Component
 {
+    [DataField("latchtime"), ViewVariables(VVAccess.ReadWrite)]
+    public float LatchTime = 1.5f;
 
+    [DataField("lungedistance"), ViewVariables(VVAccess.ReadWrite)]
+    public float LungeDistance = 50.0f;
+
+    [DataField("alertsound")]
+    public SoundSpecifier AlertSound;
 }
--- Content.Shared/SecDog/SecDogSystem.cs
+++ Content.Shared/SecDog/SecDogSystem.cs
@@ -1,6 +1,17 @@
 namespace Content.Shared.SecDog;
 
-public sealed class SecDogSystem: EntitySystem
+
+public sealed class SecDogSystem : EntitySystem
 {
 
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<SecDogComponent, ComponentShutdown>(OnShutdown);
+    }
+
+
+    public void OnShutdown(EntityUid uid, SecDogComponent component, ComponentShutdown args)
+    {
+
+    }
 }
